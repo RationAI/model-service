0c3386b0794f251182b58173da52d40b25323a53
JiriStipek <567776@mail.muni.cz>
2026-03-25 16:59:32 +0100

    fix: default tile size

--- models/binary_classifier.py
+++ models/binary_classifier.py
@@ -34,6 +34,7 @@ class BinaryClassifier:
         import lz4.frame
 
         self.lz4 = lz4.frame
+        self.tile_size = 1024  # default, will be overridden by reconfigure
 
     def reconfigure(self, config: Config) -> None:
         """Load the ONNX model and configure inference settings."""
