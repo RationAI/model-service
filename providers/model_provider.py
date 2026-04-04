27abbee9a507f6351b729941d464daaf863a9193
JiriStipek <567776@mail.muni.cz>
2026-04-04 10:25:53 +0200

    fix: remove hf model provider

--- providers/model_provider.py
+++ providers/model_provider.py
@@ -2,28 +2,3 @@ def mlflow(artifact_uri: str) -> str:
     import mlflow.artifacts
 
     return mlflow.artifacts.download_artifacts(artifact_uri=artifact_uri)
-
-
-def huggingface(repo_id: str, filename: str | None = None) -> str:
-    import os
-
-    from huggingface_hub import hf_hub_download, snapshot_download
-
-    # If HF_TOKEN is not set, we assume we're running in an offline environment and only allow loading from the local cache.
-    offline = os.environ.get("HF_TOKEN") is None
-
-    hf_home = os.environ.get("HF_HOME", "/mnt/huggingface_cache")
-    os.makedirs(hf_home, exist_ok=True)
-    os.environ["HF_HOME"] = hf_home
-
-    if filename:
-        return hf_hub_download(
-            repo_id=repo_id,
-            filename=filename,
-            local_files_only=offline,
-        )
-    else:
-        return snapshot_download(
-            repo_id=repo_id,
-            local_files_only=offline,
-        )
