34130475632898c4097be8f34b008303a54b9032
JiriStipek <567776@mail.muni.cz>
2026-04-18 21:08:53 +0200

    fix script

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 set -e
 
 python3 kustomize/components/applications/merge_applications.py
-kustomize build kustomize/overlays | kubectl apply -n rationai-jobs-ns -f -
+kubectl kustomize kustomize/overlays | kubectl apply -n rationai-jobs-ns -f -
